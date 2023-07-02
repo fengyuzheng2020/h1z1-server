a7cf390ab5472d6472f6bad619ae1bdf9dba8572
fengyuzheng <2062455226@qq.com>
2023-07-02 13:04:44 +0800

    修改mongo为空

--- scripts/demo/ZoneServer2016.js
+++ scripts/demo/ZoneServer2016.js
@@ -2,7 +2,8 @@ const H1Z1servers = require("../../h1z1-server");
 var Zone = new H1Z1servers.ZoneServer2016(
   1117,
   new Buffer.from("F70IaxuU8C/w7FPXY1ibXw==", "base64"),
-  process.env.MONGO_URL,
+  // process.env.MONGO_URL,
+  "",
   2
 );
 Zone.start();
--- scripts/demo/loginserver2016.js
+++ scripts/demo/loginserver2016.js
@@ -1,7 +1,8 @@
 const H1Z1servers = require("../../h1z1-server");
 var server = new H1Z1servers.LoginServer(
   1115, // <- server port
-  process.env.MONGO_URL
+    // process.env.MONGO_URL,
+    "",
 );
 server._protocol = new H1Z1servers.LoginProtocol("LoginUdp_11");
 server.start();
